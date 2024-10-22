da852aae1dc5d97eff78661ed21604f669a61916
ruiiary <guidews39@gmail.com>
2024-10-22 14:47:13 +0900

    chore: assets 파일 추가

--- harudoyak/src/pages/grow-up-record/index.tsx
+++ harudoyak/src/pages/grow-up-record/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const GrowUpRecordHome: React.FC = () => {
-  return <div>GrowUpRecordHome</div>;
+  return (
+    <>
+    <h3>오늘의 도약을 기록해 주세요.</h3>
+    <p>오늘의 감정</p>
+    
+  </>
+  )
 };
 
 export default GrowUpRecordHome;
