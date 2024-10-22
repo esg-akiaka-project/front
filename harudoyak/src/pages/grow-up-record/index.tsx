577a397813b5331c59fe3d281d929e43e46566e2
ruiiary <guidews39@gmail.com>
2024-10-22 15:29:27 +0900

    feat: emotion img 버튼

--- harudoyak/src/pages/grow-up-record/index.tsx
+++ harudoyak/src/pages/grow-up-record/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Emotions from './Emotions'
 
 const GrowUpRecordHome: React.FC = () => {
   return (
-    <>
+  <>
     <h3>오늘의 도약을 기록해 주세요.</h3>
     <p>오늘의 감정</p>
-    
+    <Emotions/>
   </>
   )
 };
