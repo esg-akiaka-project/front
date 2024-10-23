dc82dc455c8ffd9abe3a246177044766575b42ea
minjae-bae <2240bmj@naver.com>
2024-10-23 15:33:19 +0900

    feature: alarm (jay)

--- harudoyak/src/pages/alarm/index.tsx
+++ harudoyak/src/pages/alarm/index.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import GeneralAlarm from "./general-alarm";
+import CommunityAlarm from "./community-alarm";
 
 const AlarmHome: React.FC = () => {
-  return <div>AlarmHome</div>;
+  const [activeTab, setActiveTab] = useState<string>("Record");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+  const styles = {
+    container: {
+      maxWidth: "400px",
+      margin: "20px auto",
+      padding: "20px",
+      backgroundColor: "#F2F6F3",
+    },
+    ButtonContainer: {
+      display: "flex",
+      justifyContent: "space-between",
+      marginBottom: "20px",
+    },
+    Button: (isActive: boolean) => ({
+      flex: "1",
+      padding: "10px",
+      backgroundColor: isActive ? "#3C7960" : "#A5CBBC",
+      color: "#ffffff",
+      border: "none",
+      borderRadius: "5px",
+    }),
+  };
+  return (
+    <div style={styles.container}>
+      <div style={styles.ButtonContainer}>
+        <button
+          style={styles.Button(activeTab === "Record")}
+          onClick={() => handleTabClick("Record")}
+        >
+          도약기록
+        </button>
+        <button
+          style={styles.Button(activeTab === "Community")}
+          onClick={() => handleTabClick("Community")}
+        >
+          서로도약
+        </button>
+      </div>
+      <div>
+        {activeTab === "Record" ? (
+          <GeneralAlarm />
+        ) : (
+          <CommunityAlarm/>
+        )}
+      </div>
+    </div>
+  );
 };
+
 
 export default AlarmHome;
