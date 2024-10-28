31a72bd239680d77c2066a8d2551dd7261f9ba43
minjae-bae <2240bmj@naver.com>
2024-10-28 09:51:41 +0900

    modify on wed

--- harudoyak/src/pages/alarm/index.tsx
+++ harudoyak/src/pages/alarm/index.tsx
@@ -1,6 +1,4 @@
-import React, {useState} from "react";
-import GeneralAlarm from "./general-alarm";
-import CommunityAlarm from "./community-alarm";
+import React, { useState } from "react";
 
 const AlarmHome: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>("Record");
@@ -8,6 +6,41 @@ const AlarmHome: React.FC = () => {
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
+
+  const GeneralAlarmData = [
+    {
+      id: "성장기록",
+      type: "성장기록 알람",
+      content: "결과 요약",
+      date: "2024-10-23",
+    },
+    {
+      id: "AI편지",
+      type: "편지 도착 알림",
+      content: "도약이의 편지가 도착했습니다.",
+      date: "2024-10-23",
+    },
+  ];
+
+  const CommunityAlarmData = [
+    {
+      id: "신규댓글",
+      type: "신규댓글 알람",
+      content: "댓글 요약",
+      date: "2024-10-23",
+    },
+  ];
+
+  const [clickedAlarmCard, setClickedAlarmCard] = useState(
+    new Array(GeneralAlarmData.length).fill(false)
+  );
+
+  const handleCardClick = (index: number) => {
+    const updatedClickedAlarmCard = [...clickedAlarmCard];
+    updatedClickedAlarmCard[index] = true;
+    setClickedAlarmCard(updatedClickedAlarmCard);
+  };
+
   const styles = {
     container: {
       maxWidth: "400px",
@@ -28,7 +61,25 @@ const AlarmHome: React.FC = () => {
       border: "none",
       borderRadius: "5px",
     }),
+    AlarmCard: (isClicked: boolean) => ({
+      padding: "15px",
+      marginBottom: "10px",
+      borderRadius: "5px",
+      cursor: "pointer",
+      backgroundColor: isClicked ? "rgba(121, 116, 126, 0.08)" : "rgba(110, 173, 107, 0.3)",
+    }),
+    CardTitle: {
+      fontWeight: "bold",
+    },
+    CardContent: {
+      margin: "5px 0",
+    },
+    CardDate: {
+      fontSize: "12px",
+      color: "#666",
+    },
   };
+
   return (
     <div style={styles.container}>
       <div style={styles.ButtonContainer}>
@@ -46,15 +97,32 @@ const AlarmHome: React.FC = () => {
         </button>
       </div>
       <div>
-        {activeTab === "Record" ? (
-          <GeneralAlarm />
-        ) : (
-          <CommunityAlarm/>
-        )}
+        {activeTab === "Record"
+          ? GeneralAlarmData.map((alarmCard, index) => (
+              <div
+                key={alarmCard.id}
+                onClick={() => handleCardClick(index)}
+                style={styles.AlarmCard(clickedAlarmCard[index])}
+              >
+                <div style={styles.CardTitle}>{alarmCard.type}</div>
+                <div style={styles.CardContent}>{alarmCard.content}</div>
+                <div style={styles.CardDate}>{alarmCard.date}</div>
+              </div>
+            ))
+          : CommunityAlarmData.map((alarmCard, index) => (
+              <div
+                key={alarmCard.id}
+                onClick={() => handleCardClick(index)}
+                style={styles.AlarmCard(clickedAlarmCard[index])}
+              >
+                <div style={styles.CardTitle}>{alarmCard.type}</div>
+                <div style={styles.CardContent}>{alarmCard.content}</div>
+                <div style={styles.CardDate}>{alarmCard.date}</div>
+              </div>
+            ))}
       </div>
     </div>
   );
 };
-
 
 export default AlarmHome;
