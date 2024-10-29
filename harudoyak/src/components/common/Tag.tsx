d851402e4d298952bdecca80353076d0f9b50483
ruiiary <guidews39@gmail.com>
2024-10-29 14:02:44 +0900

    chore: common component 설명 주석 추가

--- harudoyak/src/components/common/Tag.tsx
+++ harudoyak/src/components/common/Tag.tsx
@@ -1,3 +1,12 @@
+// 컴포넌트 이름: Tag
+// 컴포넌트 설명: 도약태그 1개 
+// 주 사용처: 도약기록 쓰기, 도약기록 확인 기능에서 활용 -> Tag.tsx는 태그 딱 하나라서, 가로로 정렬된 태그들을 사용하고 싶으시면 Tags.tsx 파일을 사용하시면 됩니다!
+// 저장 위치: src/components/commom/Tag.tsx
+
+// Dev Log
+// 최초 작성일/작성자: 2024.10.29./루이
+// 수정일/작성자: 
+
 import React from "react";
 import styled from "styled-components";
 
