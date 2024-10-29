97733d14fa4ffa3f409b5452f40268a9298035fc
jaehyeok <sgn07108@gmail.com>
2024-10-30 01:43:54 +0900

    community/selectpicture

---
+++ harudoyak/src/components/community/MainPhoto.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface MainPhotoProps {
+    selectedPhoto: string | null;
+}
+
+const MainPhotoContainer = styled.div`
+    width: 100%;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f0f0f0;
+`;
+
+const Photo = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+export const MainPhoto: React.FC<MainPhotoProps> = ({ selectedPhoto }) => {
+    return (
+        <MainPhotoContainer>
+            {selectedPhoto ? (
+                <Photo src={selectedPhoto} alt="Selected" />
+            ) : (
+                <p>가장 최근에 찍은 사진이 여기에 표시됩니다.</p>
+            )}
+        </MainPhotoContainer>
+    );
+};
---
+++ harudoyak/src/pages/community/select-comment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Root from "../../style/Root";
+
+const CommunityHome: React.FC = () => {
+  return (
+    <Root>
+      <div>selectpicture</div>
+    </Root>
+  );
+};
+
+export default CommunityHome;
