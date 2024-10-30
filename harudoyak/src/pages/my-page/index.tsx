401c4d4f704c4e448e5386c7e1aa03a1fff6e98c
psi7218 <psi7218@naver.com>
2024-10-30 09:18:54 +0900

    마이페이지 개발

--- harudoyak/src/pages/my-page/index.tsx
+++ harudoyak/src/pages/my-page/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Root from "../../style/Root";
+import UndoXButton from "@/src/components/buttons/UndoXButton";
 
 const MyPageHome: React.FC = () => {
   return (
     <Root>
       <div>MyPageHome</div>
+      <UndoXButton />
     </Root>
   );
 };
