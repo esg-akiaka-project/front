6e72ab987025fc7a68c825516959ce7a01073825
psi7218 <psi7218@naver.com>
2024-10-30 09:31:00 +0900

    Fix: UndoButton 수정

--- harudoyak/src/pages/my-page/index.tsx
+++ harudoyak/src/pages/my-page/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Root from "../../style/Root";
 import UndoXButton from "@/src/components/buttons/UndoXButton";
-
+import icon_arrow from "@/public/assets/common/icon_arrow.svg";
 const MyPageHome: React.FC = () => {
   return (
     <Root>
       <div>MyPageHome</div>
-      <UndoXButton />
+      <UndoXButton icon={icon_arrow} />
     </Root>
   );
 };
