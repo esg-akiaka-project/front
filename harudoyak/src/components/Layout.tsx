c20cbe09d2322c210748e2cc699b45014dd8fcc2
psi7218 <psi7218@naver.com>
2024-10-31 12:51:09 +0900

    feature: navbar global 적용

---
+++ harudoyak/src/components/Layout.tsx
@@ -0,0 +1,33 @@
+//Navbar를 global하게 구성하기 위한 component
+// components/Layout.tsx
+import React from "react";
+import NavigationBar from "./common/navigationbar/NavigationBar";
+import { useRouter } from "next/router";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  // 내비게이션 바를 제외할 페이지 경로 설정
+  const noNavPages = [
+    currentPath.startsWith("/log-in"), // 로그인 및 회원가입 하위 페이지는 모두 제외
+    currentPath.startsWith("/sign-up"),
+    // 만약 특정 페이지만 제외하고싶은경우,
+    // ex) community 에서 community/detail만 제외하고싶다면
+    // currentPath === "community/detail" 모든 주소를 작성해서 추가하면 됨
+  ];
+  const isNoNavPage = noNavPages.some((condition) => condition);
+
+  return (
+    <div>
+      {children}
+      {!isNoNavPage && <NavigationBar />}
+    </div>
+  );
+};
+
+export default Layout;
