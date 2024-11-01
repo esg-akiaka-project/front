3e3dc8ebb3b2deb9f122e3db3690b859adb60abb
psi7218 <psi7218@naver.com>
2024-11-01 17:57:11 +0900

    feature: api-instance 및 zustand 기초설정

---
+++ harudoyak/src/apis/axiosInstance.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "백엔드 api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    // 요청 전달 전 특정 작업 수행
+    // ex) accesstoken 지정
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  // 실패 응답 반환시 작업 수행
+  // accessToken 만료의 경우, refresh를 기반으로 재발급 이후 다시 요청 보내기
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
