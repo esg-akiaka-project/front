09522bc111e1b78399582977fc3d2695dc431a02
rmkim7 <rmjkim7@gmail.com>
2024-11-03 22:27:37 +0900

    style: 로고 이미지 추가 / HomeLoginButton 수정

--- harudoyak/src/components/home/HomeLoginButton.tsx
+++ harudoyak/src/components/home/HomeLoginButton.tsx
@@ -2,41 +2,42 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
-const Button = styled.button`
-
-border: none;
-display: flex;
-
-width: 100%;
-height: 48px;
-justify-content: center;
-align-items: center;
-align-self: stretch;
-border-radius: 24px;
-background: var(--main-green);
-
-color: var(--white-from-grayscale);
-font-family: Roboto;
-font-size: 16px;
-font-style: normal;
-font-weight: 500;
-line-height: 100%; /* 16px */
-letter-spacing: 0.5px;
-
-  cursor: pointer;
-  margin-bottom: 2rem;
-
-  &:hover {
-    background-color: var(--sub-green1);
-  }
-`;
-
 const LoginButton: React.FC = () => {
   const router = useRouter();
 
   return (
-    <Button onClick={() => router.push('/log-in') }>로그인</Button>
+    <Wrapper>
+      <Button onClick={() => router.push('/log-in') }>로그인</Button>
+    </Wrapper>
   );
 };
 
 export default LoginButton;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  width: 300px;
+  height: 48px;
+  border-radius: 24px;
+  background: var(--main-green);
+  margin-bottom: 2rem;
+
+  color: var(--white-from-grayscale);
+  font-family: Inter;
+  font-size: 1rem;
+  font-weight: bold;
+  letter-spacing: 0.5px;
+  text-align: center; 
+
+  cursor: pointer;
+  
+  &:hover {
+    background-color: var(--sub-green1);
+  }
+`;
