d9fc1773b9917af56bc88cdd7cf104b5c3b3ce14
psi7218 <psi7218@naver.com>
2024-11-19 09:51:25 +0900

    fix: 알람 수정

--- harudoyak/src/components/alarm/TabButtonContainer.tsx
+++ harudoyak/src/components/alarm/TabButtonContainer.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import TapButton from './TapbuttonSet';  // TapButtonSet 임포트
+import React from "react";
+import styled from "styled-components";
+import TapButton from "./TapbuttonSet"; // TapButtonSet 임포트
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -16,14 +16,16 @@ const TapButtonContainer: React.FC<{
   return (
     <ButtonContainer>
       {/* TapButton 컴포넌트에 label prop 전달 */}
-      <TapButton 
-        isActive={activeTab === "Record"} 
-        label="도약 기록"  // label prop 추가
-        onClick={() => handleTabClick("Record")} />
-      <TapButton 
-        isActive={activeTab === "Community"} 
-        label="서로 도약"  // label prop 추가
-        onClick={() => handleTabClick("Community")} />
+      <TapButton
+        $isActive={activeTab === "Record"}
+        label="도약 기록" // label prop 추가
+        onClick={() => handleTabClick("Record")}
+      />
+      <TapButton
+        $isActive={activeTab === "Community"}
+        label="서로 도약" // label prop 추가
+        onClick={() => handleTabClick("Community")}
+      />
     </ButtonContainer>
   );
 };
